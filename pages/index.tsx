cdb72019dbde888aef2073a827c43f6784a0ee6c
Mr.Worldwide <syah@worqapp.com>
2021-07-08 04:38:59 +0800

    UI on add User && update list when user successfully added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import {
   Container,
   Typography,
+  TextField,
+  Button,
   Paper,
   TableContainer,
   Table,
@@ -9,6 +11,7 @@ import {
   TableRow,
   TableBody,
   TableCell,
+  useTheme,
 } from "@material-ui/core";
 import request from "util/request";
 
@@ -21,20 +24,59 @@ interface User {
 
 const Home: React.FC = () => {
   let [userList, setUserList] = useState<User[]>([]);
-  useEffect(() => {
-    const requestUser = async () => {
-      try {
-        const requestOption: RequestInit = {
-          method: `GET`,
-        };
-        const response = await request<User[]>(`/user`, requestOption);
-        if (response.parsedBody) {
-          setUserList(response.parsedBody);
-        }
-      } catch (err) {
-        console.log(err);
+  let [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    phone: "",
+  });
+
+  const theme = useTheme();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const addUser = async () => {
+    try {
+      const requestOption: RequestInit = {
+        method: `POST`,
+        headers: {
+          "Content-Type": `application/json`,
+        },
+        body: JSON.stringify({ ...userData }),
+      };
+      const response = await request<User[]>(`/user`, requestOption);
+      if (response.parsedBody) {
+        setUserData({
+          username: "",
+          email: "",
+          phone: "",
+        });
+        requestUser();
       }
-    };
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const requestUser = async () => {
+    try {
+      const requestOption: RequestInit = {
+        method: `GET`,
+      };
+      const response = await request<User[]>(`/user`, requestOption);
+      if (response.parsedBody) {
+        setUserList(response.parsedBody);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
     requestUser();
   }, []);
   return (
@@ -56,6 +98,48 @@ const Home: React.FC = () => {
       >
         Let&lsquo;s have some fun!!!
       </Typography>
+      <Paper style={{ marginBottom: `${theme.spacing(2)}px` }}>
+        <form noValidate autoComplete="off">
+          <TextField
+            id="name"
+            name="username"
+            placeholder="Name"
+            label="Name"
+            variant="outlined"
+            InputLabelProps={{ shrink: true }}
+            value={userData.username}
+            onChange={handleChange}
+          />
+          <TextField
+            id="email"
+            name="email"
+            placeholder="E-mail"
+            label="E-mail"
+            variant="outlined"
+            InputLabelProps={{ shrink: true }}
+            value={userData.email}
+            onChange={handleChange}
+          />
+          <TextField
+            id="phone"
+            name="phone"
+            placeholder="Phone No."
+            label="Phone No."
+            variant="outlined"
+            InputLabelProps={{ shrink: true }}
+            value={userData.phone}
+            onChange={handleChange}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={() => addUser()}
+          >
+            Add
+          </Button>
+        </form>
+      </Paper>
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
           <TableHead>
