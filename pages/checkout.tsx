4ea7fbea65af77110f3e1159814d196ae733d08e
Mr.Worldwide <syah@worqapp.com>
2021-07-08 14:43:35 +0800

    Create Checkout and 404 Page

---
+++ pages/checkout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+const CheckoutPage: React.FC = () => {
+  return <Container component="main">Checkout Page</Container>;
+};
+export default CheckoutPage;
