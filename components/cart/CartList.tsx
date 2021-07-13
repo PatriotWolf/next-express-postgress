f3328ffc559ca78471761b7019b08bd928ab76ac
Mr.Worldwide <syah@worqapp.com>
2021-07-13 10:42:11 +0800

    remove MobX observer as separation between Container and Component concern

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -5,7 +5,6 @@ import {
   ListItemText,
   ListItemSecondaryAction,
 } from "@material-ui/core";
-import { observer } from "mobx-react";
 
 import CartAction from "./CartAction";
 import ShoppingCartEntry from "store/cartStore/cartEntry";
@@ -15,38 +14,38 @@ interface CartListComponentProps {
   removeCart: (arg: string) => void;
 }
 
-const CartList: React.FC<CartListComponentProps> = observer(
-  ({ productList, removeCart }) => {
-    return (
-      <List>
-        {productList.length > 0 ? (
-          productList.map((entry: ShoppingCartEntry) => (
-            <ListItem key={`cart-${entry.product.id}`}>
-              <ListItemText
-                primary={entry.product.name}
-                secondary={`$${entry.price.toFixed(2)}`}
+const CartList: React.FC<CartListComponentProps> = ({
+  productList,
+  removeCart,
+}) => {
+  return (
+    <List>
+      {productList.length > 0 ? (
+        productList.map((entry: ShoppingCartEntry) => (
+          <ListItem key={`cart-${entry.product.id}`}>
+            <ListItemText
+              primary={entry.product.name}
+              secondary={`$${entry.price.toFixed(2)}`}
+            />
+            <ListItemSecondaryAction>
+              <CartAction
+                amount={entry.amount}
+                handleAddQuatity={() => entry.increaseAmount()}
+                handleReduceQuatity={() =>
+                  entry.amount > 1
+                    ? entry.decreaseAmount()
+                    : removeCart(entry.product.id)
+                }
               />
-              <ListItemSecondaryAction>
-                <CartAction
-                  amount={entry.amount}
-                  handleAddQuatity={() => entry.increaseAmount()}
-                  handleReduceQuatity={() =>
-                    entry.amount > 1
-                      ? entry.decreaseAmount()
-                      : removeCart(entry.product.id)
-                  }
-                />
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))
-        ) : (
-          <ListItem>
-            <ListItemText primary="Cart is Empty" />
+            </ListItemSecondaryAction>
           </ListItem>
-        )}
-      </List>
-    );
-  }
-);
-
+        ))
+      ) : (
+        <ListItem>
+          <ListItemText primary="Cart is Empty" />
+        </ListItem>
+      )}
+    </List>
+  );
+};
 export default CartList;
